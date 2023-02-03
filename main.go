2aa6e6a493a08e937137dadced2063a542a96f64
Will Bollock <wbollock@linode.com>
2023-02-03 08:59:46 -0500

    feat: logfmt output

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"flag"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type RaidLog struct {
-	SeqNum      string   `json:"seqNum"`
-	Time        string   `json:"Time"`
-	Code        string   `json:"Code"`
-	Class       int      `json:"Class"`
-	Locale      string   `json:"Locale"`
-	Description string   `json:"EventDescription"`
-	Data        struct{} `json:"EventData"`
+	SeqNum      string `json:"seqNum"`
+	Time        string `json:"Time"`
+	Code        string `json:"Code"`
+	Class       int    `json:"Class"`
+	Locale      string `json:"Locale"`
+	Description string `json:"EventDescription"`
+	Data        string `json:"EventData"`
 }
 
 // this program should run forever and do:
@@ -28,11 +30,13 @@ type RaidLog struct {
 // also need config file with like, storcli file location maybe? or just read from /etc/default/storcli_logger ?
 
 // maybe use type=latest=N here?
-func convertStorcliOutput() {
-	storcliPath := "/usr/sbin/storcli"
-	raidGroup := "/c0"
+func convertStorcliOutput(storcliPath string, raidGroup string, latest int) {
+
+	// e.g storcli /c0 show events type=latest=5
+	// gets last 5 events
+	latestArg := "type=latest=" + strconv.Itoa(latest)
 	// get event log
-	out, err := exec.Command(storcliPath, raidGroup, "show events").Output()
+	out, err := exec.Command(storcliPath, raidGroup, "show events", latestArg).Output()
 
 	if err != nil {
 		log.Fatal(err)
@@ -57,7 +61,7 @@ func convertStorcliOutput() {
 		// split the individual raidlog into distinct events
 		lines := strings.Split(event, "\n")
 
-		for _, line := range lines {
+		for counter, line := range lines {
 			if strings.HasPrefix(line, "seqNum:") {
 				raidlog.SeqNum = strings.TrimSpace(line[len("seqNum:"):])
 			} else if strings.HasPrefix(line, "Time:") {
@@ -72,22 +76,72 @@ func convertStorcliOutput() {
 				raidlog.Locale = strings.TrimSpace(line[len("Locale:"):])
 			} else if strings.HasPrefix(line, "Event Description:") {
 				raidlog.Description = strings.TrimSpace(line[len("Event Description:"):])
+			} else if strings.HasPrefix(line, "Event Data:") {
+				// eventData is everything after EventData up until "CLI Version = " (the last event)
+				// so if we see CLI Version =, we've gone too far and should just discard everything
+				eventData := strings.Split(strings.Join(lines[counter:], ";"), "CLI Version =")
+				raidlog.Data = eventData[0]
+
 			}
 
 			raidlogs = append(raidlogs, raidlog)
 		}
 	}
 
-	raidJSON, err := json.Marshal(raidlogs)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	for _, raid := range raidlogs {
+		log.WithFields(log.Fields{
+			"SeqNum":            raid.SeqNum,
+			"Time":              raid.Time,
+			"Code":              raid.Code,
+			"Class":             raid.Class,
+			"Locale":            raid.Locale,
+			"Event Description": raid.Description,
+			"Event Data":        raid.Data,
+		}).Info()
 
-	fmt.Println(string(raidJSON))
+	}
 
 }
 
+func getMaxEvents(storcliPath string, raidGroup string) (latest int) {
+
+	// get event log
+	out, err := exec.Command(storcliPath, raidGroup, "show events").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	latest = strings.Count(string(out), "seqNum")
+	return latest
+}
+
 func main() {
-	convertStorcliOutput()
+	var (
+		storcliPath = flag.String("storcli.path", "/usr/sbin/storcli",
+			"Storcli Binary Path")
+		raidGroup = flag.String("raidgroup", "/c0",
+			"Storcli raid group (/cx)")
+		logPath = flag.String("log.path", "/var/log/storcli.log",
+			"Desired path to log to")
+	)
+
+	f, err := os.OpenFile(*logPath, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(f)
+
+	// on program first start or restart
+	max := getMaxEvents(*storcliPath, *raidGroup)
+	convertStorcliOutput(*storcliPath, *raidGroup, max)
+	var latest, newMax int
+	for {
+		latest = max
+		if latest != 0 {
+			convertStorcliOutput(*storcliPath, *raidGroup, latest)
+		}
+		// we only want to get events that are new since our last run
+		newMax = getMaxEvents(*storcliPath, *raidGroup)
+		max = newMax - max
+		time.Sleep(20 * time.Second)
+	}
 }
